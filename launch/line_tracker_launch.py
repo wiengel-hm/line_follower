7f17b90672740be669f4ee44965a9b4717332f6a
william-mx <william.engel@mdynamix.de>
2025-05-11 20:51:30 +0000

    add rqt image node

--- launch/line_tracker_launch.py
+++ launch/line_tracker_launch.py
@@ -1,22 +1,29 @@
 import launch
 import launch_ros.actions
+from launch_ros.actions import Node
 
 from ament_index_python.packages import get_package_share_directory
 
 def generate_launch_description():
-    # Path to the configuration file for the line follower node
-    config_file_path = get_package_share_directory('line_follower') + '/config/config_lane_640x360.yaml'
     
     # Follower node declaration
-    line_tracker = launch_ros.actions.Node(
+    line_tracker = Node(
         package='line_follower',
         executable='line_tracker',
         name='line_tracker',
-        output='screen',
-        parameters=[config_file_path]
+        output='screen'
+    )
+
+    # Launch rqt_image_view to visualize image topics
+    rqt_image_view_node = Node(
+        package="rqt_image_view",
+        executable="rqt_image_view",
+        name="rqt_image_view",
+        output="screen"
     )
 
     # Return launch description including both nodes
     return launch.LaunchDescription([
-        line_tracker
+        line_tracker,
+        rqt_image_view_node
     ])
