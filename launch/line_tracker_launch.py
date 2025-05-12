4af312ff6116f9b35ac6906cebd2484128a40aad
william-mx <william.engel@mdynamix.de>
2025-05-12 11:46:19 +0200

    add namespace

--- launch/line_tracker_launch.py
+++ launch/line_tracker_launch.py
@@ -1,18 +1,24 @@
 import launch
-import launch_ros.actions
+from launch_ros.actions import Node
+from launch.substitutions import EnvironmentVariable
 
 from ament_index_python.packages import get_package_share_directory
 
 def generate_launch_description():
     # Path to the configuration file for the line follower node
     config_file_path = get_package_share_directory('line_follower') + '/config/config_lane_640x360.yaml'
-    
+
+    # Define a substitution that reads the 'ROBOT_NAMESPACE' environment variable.
+    # If the environment variable is not set, it defaults to an empty string (global namespace).
+    robot_namespace_env = EnvironmentVariable('ROBOT_NAMESPACE', default_value = '')
+
     # Follower node declaration
-    line_tracker = launch_ros.actions.Node(
+    line_tracker = Node(
         package='line_follower',
         executable='line_tracker',
         name='line_tracker',
         output='screen',
+        namespace=robot_namespace_env,
         parameters=[config_file_path]
     )
 
